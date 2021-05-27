71dffd4265e9cad14301749195d3a9035767b119
Andrew Hughes <andrew.hughes@gofluid.org>
2021-05-27 11:51:44 +0100

    add costs object and section

--- pages/index.vue
+++ pages/index.vue
@@ -14,7 +14,7 @@
     </section>
     <section id="how-it-works">
       <div class="preview">
-        <div :class="{ fixed: fixed, hide: !showPreview }" class="screen-mock">
+        <div :class="{ fixed: fixed, hide: !showPreview, remove: removePreview }" class="screen-mock">
           <img src="/images/mac-mock.png" class="mac-mock" />
           <div class="screen">
             <img
@@ -41,8 +41,26 @@
         </div>
       </div>
     </section>
-    <section id="pricing">
-      <div class="details"></div>
+    <section id="pricing" :class="{ displayed: !largePreview }">
+      <div class="details">
+        <div class="price-table">
+          <div class="type-toggle">
+            <span :class="{ active: filter == 'onetime' }" @click.stop.prevent="filter = 'onetime'"
+              >One-Time payments</span
+            >
+            <span :class="{ active: filter == 'ongoing' }" @click.stop.prevent="filter = 'ongoing'"
+              >Ongoing payments</span
+            >
+          </div>
+          <div class="contents">
+            <div class="heading-row">
+              <span class="title">{{ filter == 'ongoing' ? 'Ongoing Payments' : 'One-Time Payments' }}</span>
+              <span class="cost">Cost</span>
+            </div>
+            <div class="rows"></div>
+          </div>
+        </div>
+      </div>
       <div class="breakdown"></div>
     </section>
     <section id="faqs"></section>
@@ -54,12 +72,17 @@
 export default {
   async asyncData({ $content }) {
     const steps = await $content('steps').fetch()
+    const costs = await $content('costs').fetch()
 
     return {
       steps,
+      costs,
       fixed: false,
       focusIndex: null,
       showPreview: true,
+      removePreview: false,
+      largePreview: true,
+      filter: 'onetime',
     }
   },
   methods: {
@@ -99,11 +122,22 @@ export default {
       let scrollT = document.documentElement.scrollTop
       let preview = document.querySelector('#how-it-works > .preview')
       let previewBottom = preview.offsetTop + preview.clientHeight
-      let marker = previewBottom - Math.floor(document.documentElement.clientHeight * 0.9)
+      let marker = previewBottom - Math.floor(document.documentElement.clientHeight * 0.2)
       if (scrollT > marker) {
         this.showPreview = false
       } else {
         this.showPreview = true
+      }
+    },
+    pricingResize() {
+      let scrollT = document.documentElement.scrollTop
+      let pricingTop = document.querySelector('#pricing').offsetTop
+      if (scrollT >= pricingTop) {
+        this.largePreview = false
+        this.removePreview = true
+      } else {
+        this.largePreview = true
+        this.removePreview = false
       }
     },
   },
@@ -111,10 +145,12 @@ export default {
     this.fixScreen()
     this.updateFocusedIndex()
     this.togglePreview()
+    this.pricingResize()
     document.onscroll = (evt) => {
       this.fixScreen()
       this.updateFocusedIndex()
       this.togglePreview()
+      this.pricingResize()
     }
   },
 }
